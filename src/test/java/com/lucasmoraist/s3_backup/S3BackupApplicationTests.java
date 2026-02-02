c7954d9c3796cc112ada08c6b10b06c9d4f21b37
lucasmoraist <luksmnt1101@gmail.com>
2026-02-01 21:57:34 -0300

    first commit

---
+++ src/test/java/com/lucasmoraist/s3_backup/S3BackupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasmoraist.s3_backup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3BackupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
